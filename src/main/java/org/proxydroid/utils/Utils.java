2122f19e94a230cd603a9090639206f1eec92491
OrpheeGT <OrpheeGT@users.noreply.github.com>
2016-11-01 13:00:23 +0100

    add Magisk Phh's SuperUser compatibility (#20)

--- src/main/java/org/proxydroid/utils/Utils.java
+++ src/main/java/org/proxydroid/utils/Utils.java
@@ -19,7 +19,7 @@ public class Utils {
 
   public final static String TAG = "ProxyDroid";
   public final static String DEFAULT_SHELL = "/system/bin/sh";
-  public final static String DEFAULT_ROOTS[] = {"/system/bin/su", "/system/xbin/su", "/su/bin/su", "/su/xbin/su", "/sbin/su"};
+  public final static String DEFAULT_ROOTS[] = {"/system/bin/su", "/system/xbin/su", "/su/bin/su", "/su/xbin/su", "/sbin/su", "/magisk/.core/bin/su"};
   public final static String DEFAULT_IPTABLES = "/data/data/org.proxydroid/iptables";
   public final static String ALTERNATIVE_IPTABLES = "/system/bin/iptables";
   public final static int TIME_OUT = -99;

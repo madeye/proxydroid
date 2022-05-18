b8225c17e12c7c43e7adc39ab5ff69fe6b7458b5
Louiswong <louiswong2099@gmail.com>
2022-05-18 23:43:53 +0800

    fix the issue that the hotspot on newer versions of Android doesn't forward the traffic because the interface's name has changed.

--- app/src/main/assets/x86/proxy.sh
+++ app/src/main/assets/x86/proxy.sh
@@ -132,12 +132,16 @@ redsocks {
  $DIR/redsocks -p $DIR/redsocks.pid -c $DIR/redsocks.conf
  iptables -A INPUT -i ap+ -p tcp --dport 8123 -j ACCEPT
  iptables -A INPUT -i ap+ -p tcp --dport 8124 -j ACCEPT
+ iptables -A INPUT -i wlan1 -p tcp --dport 8123 -j ACCEPT
+ iptables -A INPUT -i wlan1 -p tcp --dport 8124 -j ACCEPT
  iptables -A INPUT -i lo -p tcp --dport 8123 -j ACCEPT
  iptables -A INPUT -i lo -p tcp --dport 8124 -j ACCEPT
  iptables -A INPUT -p tcp --dport 8123 -j DROP
  iptables -A INPUT -p tcp --dport 8124 -j DROP
  iptables -t nat -A PREROUTING -i ap+ -p tcp -d 192.168.43.1/24 -j RETURN
  iptables -t nat -A PREROUTING -i ap+ -p tcp -j REDIRECT --to $proxy_port
+ iptables -t nat -A PREROUTING -i wlan1 -p tcp -d 192.168.43.1/24 -j RETURN
+ iptables -t nat -A PREROUTING -i wlan1 -p tcp -j REDIRECT --to $proxy_port
  ;;
 stop)
 
@@ -146,6 +150,11 @@ stop)
  iptables -t nat -D PREROUTING -i ap+ -p tcp -j REDIRECT --to 8124
  iptables -D INPUT -i ap+ -p tcp --dport 8123 -j ACCEPT
  iptables -D INPUT -i ap+ -p tcp --dport 8124 -j ACCEPT
+ iptables -t nat -D PREROUTING -i wlan1 -p tcp -d 192.168.43.1/24 -j RETURN
+ iptables -t nat -D PREROUTING -i wlan1 -p tcp -j REDIRECT --to 8123
+ iptables -t nat -D PREROUTING -i wlan1 -p tcp -j REDIRECT --to 8124
+ iptables -D INPUT -i wlan1 -p tcp --dport 8123 -j ACCEPT
+ iptables -D INPUT -i wlan1 -p tcp --dport 8124 -j ACCEPT
  iptables -D INPUT -i lo -p tcp --dport 8123 -j ACCEPT
  iptables -D INPUT -i lo -p tcp --dport 8124 -j ACCEPT
  iptables -D INPUT -p tcp --dport 8123 -j DROP
